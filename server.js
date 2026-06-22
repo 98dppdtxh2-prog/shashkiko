284edfefc70bd0404fca4dcf3a8447813a7c25f6
servant of god <98dppdtxh2@privaterelay.appleid.com>
2026-06-22 04:34:14 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,30 +2,73 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ვინახავთ თითოეული ოთახის დაფის მდგომარეობას
 const rooms = {};
 
+// საწყისი დაფის ფუნქცია
+function createInitialBoard() {
+    let board = Array(8).fill(null).map(() => Array(8).fill(null));
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 8; c++) {
+            if ((r + c) % 2 === 1) board[r][c] = { type: 'b', king: false };
+        }
+    }
+    for (let r = 5; r < 8; r++) {
+        for (let c = 0; c < 8; c++) {
+            if ((r + c) % 2 === 1) board[r][c] = { type: 'w', king: false };
+        }
+    }
+    return board;
+}
+
 io.on('connection', (socket) => {
+    let currentRoom = null;
+
+    // ოთახში შესვლა
     socket.on('joinRoom', (roomId) => {
-        if (!rooms[roomId]) rooms[roomId] = { players: [], board: null, turn: 'w' };
-        
-        if (rooms[roomId].players.length < 2) {
-            const color = rooms[roomId].players.length === 0 ? 'w' : 'b';
-            rooms[roomId].players.push({ id: socket.id, color });
-            socket.join(roomId);
-            socket.emit('assignedColor', color);
-            if (rooms[roomId].players.length === 2) io.to(roomId).emit('startGame');
+        currentRoom = roomId;
+        socket.join(roomId);
+
+        // თუ ოთახი არ არსებობს, ვქმნით ახალ დაფას
+        if (!rooms[roomId]) {
+            rooms[roomId] = {
+                board: createInitialBoard(),
+                turn: 'w'
+            };
+        }
+
+        // ვუგზავნით ახალ მოთამაშეს მიმდინარე დაფის მდგომარეობას
+        socket.emit('initGameState', rooms[roomId]);
+    });
+
+    // სვლის გაკეთება
+    socket.on('move', (data) => {
+        if (currentRoom && rooms[currentRoom]) {
+            rooms[currentRoom].board = data.board;
+            rooms[currentRoom].turn = data.turn;
+            // ვუგზავნით სვლას მხოლოდ ამავე ოთახის წევრებს
+            socket.to(currentRoom).emit('move', data);
         }
     });
 
-    socket.on('move', (data) => {
-        socket.to(data.roomId).emit('move', data);
+    // ჩატის მესიჯი
+    socket.on('chatMessage', (msg) => {
+        if (currentRoom) {
+            socket.to(currentRoom).emit('chatMessage', msg);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        // სურვილისამებრ აქ შეიძლება ოთახის გაწმენდის ლოგიკა
     });
 });
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
